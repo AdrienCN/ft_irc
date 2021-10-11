c9696f10ad358d213c4f506bda5f35b4d3eb3a3d
adrien <adrien.constan@gmail.com>
2021-10-11 10:36:57 +0200

    error

--- ft_errors.cpp
+++ ft_errors.cpp
@@ -29,6 +29,16 @@ void ft_error(int nb, std::vector<std::string> params, Client* client, Channel* 
             error = channel->getName() + " :Cannot send to channel\r\n";
             break;
         }
+		case 405: //ERR_TOOMANYCHANNELS
+		{
+			error = ; 
+			break;
+		}
+		case 407: //ERR_TOOMANYTARGETS
+		{
+			error = ;
+			break;
+		}
         case 411: // ERR_NORECIPIENT
         {
             std::string command = params[0];
@@ -40,6 +50,56 @@ void ft_error(int nb, std::vector<std::string> params, Client* client, Channel* 
             error = ":No text to send\r\n";
             break;
         }
+		case 413: //ERR_NOTOPLEVEL
+		{
+			error = ;
+			break;
+		}
+		case 414: //ERR_WILDTOPLEVEL
+		{
+			error = ;
+			break;
+		}
+		case 415: //ERR_BADCHANMASK
+		{
+			error = ;
+			break;
+		}
+
+		case 421: //ERR_UNKNOWNCOMMAND
+		{
+			error = ;
+			break;
+		}
+		case 431: //ERR_NONICKNAMEGIVEN
+		{
+			error = ;
+			break;
+		}
+
+		case 432: //ERR_ERRONEOUSNICKNAME
+		{
+			error = ;
+			break;
+		}
+
+		case 433: //ERR_NICKNAMEINUSE
+		{
+			error = ;
+			break;
+		}
+
+		case 436: //ERR_NICKCOLLISION
+		{
+			error = ;
+			break;
+		}
+		case 437: //ERR_UNAVAILABLERESSOURCE
+		{
+			error = ;
+			break;
+		}
+
         case 442: // ERR_NOTONCHANNEL
         {
             error = channel->getName() + " :You're not on that channel\r\n";
@@ -51,6 +111,21 @@ void ft_error(int nb, std::vector<std::string> params, Client* client, Channel* 
             error = command + " :Not enough parameters\r\n";
             break;
         }
+		case 462: //ERR_ALREADYREGISTERED
+		{
+			error = ;
+			break;
+		}
+		case 471: //ERR_CHANNELISFULL
+		{
+			error = ;
+			break;
+		}
+		case 473: //ERR_INVITEONLYCHAN
+		{
+			error = ;
+			break;
+		}
         case 474: // ERR_BANNEDFROMCHANNEL
         {
             error = channel->getName() + " :Cannot join channel (+b)\r\n";
@@ -62,6 +137,11 @@ void ft_error(int nb, std::vector<std::string> params, Client* client, Channel* 
             error = channel->getName() + " :Cannot join channel (+k)\r\n";
             break;
         }
+		case 484: //ERR_RESTRICTED
+		{
+			error = ;
+			break;
+		}
         default:
 		{
 			error = "No Error set yet \r\n";
